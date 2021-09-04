2cd5028497e31f28858c6a3a90d63feaab577b5d
TheMIG1122 <themig1122@gmail.com>
2021-09-05 00:35:48 +0500

    Error Handler Completes

--- index.php
+++ index.php
@@ -4,6 +4,30 @@
     https://www.youtube.com/watch?v=8otFp9yXA5E
 */
 
-echo $s;
+// Function to overwrite error handler
+function my_error($error_number, $error_text, $filename, $line_number)
+{
+    // Storing Errors in File
+    $date = date("Y-m-d");
+
+    $error_string = $error_number . ":" . $error_text . " in" . $filename . " at line " . $line_number . "\n";
+    file_put_contents("logs/" . $date . ".txt", $error_string,FILE_APPEND);
+}
+
+
+// Method 01
+// error_reporting(0);
+// set_error_handler("my_error");
+
+// Method 02
+// ini_set("display_errors",0);
+// ini_set("log_errors",1);
+// ini_set("error_log","logs/error_log.txt");
+
+
+// Some Tests
+// include 'go.php';
+// echo $_GET['he'];
+// my_function();
 
 ?>
